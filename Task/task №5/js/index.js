3bc7b3ac87a891ebb9d2769a52b234b4a92b7f12
RomanDyukoff <hlebahleb@gmail.com>
2021-11-01 14:13:02 +0300

    start: task 6

--- Task/task №5/js/index.js
+++ Task/task №5/js/index.js
@@ -1,1 +1,12 @@
-console.log(myLibraru);
+
+let xx = [1, 2, 22, 3, -3, -10, 6]
+
+function positiveSum(arr) {
+  return arr.filter(i => i > 0).map(i => x += i, x = 0).reverse()[0]
+}
+
+function hi() {
+  return 'hello world'
+}
+
+hi()
